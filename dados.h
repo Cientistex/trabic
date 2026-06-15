5ef7534407ea03a09f297ce7695f963d8d12e4e5
MrN00b3000 <miguelsepulvida3000@gmail.com>
2026-06-15 14:08:44 -0300

    Adição de comentários no "dados.h"

--- dados.h
+++ dados.h
@@ -1,6 +1,7 @@
 #ifndef DADOS_H
 #define DADOS_H
 
+// Definição do Tipo Item
 #include <stdbool.h>
 typedef struct{
     char nome[30];
@@ -8,13 +9,15 @@ typedef struct{
     bool possui; // Indica se o jogador coletou ou não a pista  
 }Item;
 
+// Definição do Tipo Jogador
 typedef struct{
     char nome[50];
-    Item inventario[5];    
+    Item inventario[5]; //Slot 0: Bilhete, Slot 1: Chave Lab, etc.
 }Jogador;
 
+//Declaração das funções que MIGUEL irá programar (o grupo terá acesso a elas)
 void InicializarJogador(Jogador *j, const char *nome_inicial);
 bool SalvarJogo(Jogador *j);
-boolCarregarJogo(Jogador *j);
+bool CarregarJogo(Jogador *j);
 
 #endif
